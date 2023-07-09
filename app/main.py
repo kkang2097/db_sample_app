172d97941f2cba4f0f8ece8c975c72724d7e5710
Elliot Kang <kkang2097@gmail.com>
2023-07-08 22:52:38 -0700

    fix async stuff, add_rss API call

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, HTTPException
 from functools import wraps, lru_cache
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import uvicorn
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
+import re
 import config
 
 load_dotenv()
@@ -30,7 +31,7 @@ def get_db_client():
   return MongoClient(config.MONGO_URI)
 
 #Need an auth wrapper!
-def auth_required(func):
+def auth_required_async(func):
   @wraps(func)
   async def wrapper(*args, **kwargs):
     #TODO: do stuff here (ie. check header for auth, but we should do this wayyyy later...)
@@ -42,9 +43,21 @@ def auth_required(func):
     return await func(*args, **kwargs)
   return wrapper
 
+def auth_required_sync(func):
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    #TODO: do stuff here (ie. check header for auth, but we should do this wayyyy later...)
+
+    #We need our generated token string as the auth :)
+    #Optimally we have some assert statement here...
+    print(kwargs['request'].headers)
+
+    return func(*args, **kwargs)
+  return wrapper
+
 #Dummy HTTP request sample
 @app.get('/')
-@auth_required
+@auth_required_async
 async def root(request: Request):
   #return {'example': 'This is an example', 'data': config.FRONTEND_PW}
   return {'example': 'This is an example', 'data': 'fooled you'}
@@ -55,7 +68,7 @@ async def root(request: Request):
 
 #Check MongoDB connection
 @app.get('/checkdb')
-@auth_required
+@auth_required_async
 async def check_db(request: Request, client: MongoClient = Depends(get_db_client)):
   retVal = client.admin.command('ping')
   print("Pinged your deployment. You successfully connected to MongoDB!")
@@ -64,23 +77,71 @@ async def check_db(request: Request, client: MongoClient = Depends(get_db_client
 
 #Get user
 #This should actually be a POST request
-@app.get('/getuser')
-@auth_required
-async def get_user(request: Request, client: MongoClient = Depends(get_db_client)):
-  # This is how to get a part of the header!
-  #return request.headers['cookie']
+@app.post('/getuser')
+@auth_required_sync
+def get_user(username: str, request: Request, client: MongoClient = Depends(get_db_client)):
+  #Input:
+  # Username - String
+  #Output:
+  # Success (hopefully)
+
+  user_coll = client['data']['users']
+  outcome = user_coll.find_one({'_id': username})
 
   #Get the user, if not we just create one.
-  return {'answer': 'helloooo'}
+  if(outcome == None):
+    outcome = user_coll.insert_one({'_id': username,
+                                    'subs': {'dummy_sub1': 'dummy_desc', 'dummy_sub2': 'dummy_desc'},
+                                    'feed': {'dummy_post': 'dummy_body', 'dummy_post2': 'dummy_body2'},
+                                    'curated': {'dummy_post': 'dummy_body', 'dummy_post2': 'dummy_body2'}})
+    outcome = user_coll.find_one({'_id': username})
+    return outcome
+  else:
+    return outcome
 
 #RSS calls
 @app.post('/add-rss-feed')
-@auth_required
-async def add_rss(request: Request, client: MongoClient = Depends(get_db_client)):
+@auth_required_sync
+def add_rss(rss_name: str, rss_url: str, request: Request, client: MongoClient = Depends(get_db_client)):
 
-  #await some db action
-  #TODO: Do this after specifying User stuff....
-  #response = await something
+  #Validate URL first...
+  regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+  
+  is_valid = re.match(regex, rss_url) is not None
 
-  #Conditional response - true/false
-  return {'answer': 'Feed added'}
+  #If URL is not valid, raise an error...
+  if not is_valid:
+    raise HTTPException(status_code=404, detail='URL not valid')
+
+  #Try finding the RSS Feed URL
+  user_coll = client['data']['rss_feeds']
+  outcome = user_coll.find_one({'_id': rss_url})
+
+  #Get the user, if not we just create one.
+  if(outcome == None):
+    outcome = user_coll.insert_one({'_id': rss_url, 'name': rss_name})
+    outcome = user_coll.find_one({'_id': rss_url})
+    return outcome
+  else:
+    return outcome
+
+@app.post('/add-rss-sub')
+@auth_required_sync
+def add_rss_sub(rss_name: str, username: str, request: Request, client: MongoClient = Depends(get_db_client)):
+  #TODO: Add RSS subscriptions to a specific user
+
+
+  #Find the RSS feed in our master collection
+
+
+  #If valid, add to our user's RSS subscriptions
+
+
+  
+  return None
