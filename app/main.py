c39f842fd408b2e821f5d709217338f4a98347cb
Elliot Kang <kkang2097@gmail.com>
2023-07-22 02:58:35 -0700

    Carefully making the data scheduled job

--- app/main.py
+++ app/main.py
@@ -4,9 +4,12 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import uvicorn
 from dotenv import load_dotenv
+import urllib.robotparser as urp
 from fastapi.middleware.cors import CORSMiddleware
 import config
-from .api_utils import url_is_valid
+from .api_utils import *
+import requests as req
+from datetime import datetime as dt
 
 #Load environment variables
 load_dotenv()
@@ -32,6 +35,14 @@ app.add_middleware(
 def get_db_client():
   #Connect to mongodb client
   return MongoClient(config.MONGO_URI)
+
+@lru_cache()
+def get_req_session():
+  return req.Session()
+
+@lru_cache()
+def get_url_parser():
+  return urp.RobotFileParser()
 
 #Need an auth wrapper!
 def auth_required_async(func):
@@ -105,10 +116,12 @@ def get_user(username: str, request: Request, client: MongoClient = Depends(get_
 #RSS calls
 @app.post('/add-rss-feed')
 @auth_required_sync
-def add_rss(rss_name: str, rss_url: str, request: Request, client: MongoClient = Depends(get_db_client)):
+def add_rss(rss_name: str, rss_url: str, request: Request, client: MongoClient = Depends(get_db_client), rp= Depends(get_url_parser)):
   #If URL is not valid, raise an error...
   if not url_is_valid(rss_url):
     raise HTTPException(status_code=404, detail='URL not valid')
+  if not can_scrape(rss_url, rp):
+    raise HTTPException(status_code=418, detail='Cannot scrape this URL!')
 
   #Try finding the RSS Feed URL
   user_coll = client['data']['rss_feeds']
@@ -116,7 +129,7 @@ def add_rss(rss_name: str, rss_url: str, request: Request, client: MongoClient =
 
   #Get the user, if not we just create one.
   if(outcome == None):
-    outcome = user_coll.insert_one({'_id': rss_url, 'name': rss_name})
+    outcome = user_coll.insert_one({'_id': rss_url, 'name': rss_name, 'date_added': dt.now()})
     outcome = user_coll.find_one({'_id': rss_url})
     return outcome
   else:
@@ -138,11 +151,43 @@ def add_rss_sub(rss_name: str, username: str, request: Request, client: MongoCli
 
 #Dummy function for scheduled RSS master collection reset
 #TODO: Turn this into a DAG, deploy a Docker container
-def dummy_scheduled_job(request: Request):
+@app.get('/data_scheduled')
+@auth_required_sync
+def dummy_scheduled_job(request: Request, client: MongoClient = Depends(get_db_client)):
   #If we finish, we can return True
   #Else, we can return False
 
   #Collect and parse all RSS feeds, put in master collection for posts
 
   #Grab the master RSS post collection, put into each individual collection
-  return None
+  try:
+    user_coll = client['data']['rss_feeds']
+    posts_coll = client['data']['posts']
+    all_feeds = user_coll.find({})
+    #Get all feeds
+    for f in all_feeds:
+      feed_url=f['_id']
+      articles = get_articles(feed_url)
+      #Get all articles in 1 feed
+      for a in articles:
+        title = a.find('title').text
+        link = a.find('link').text
+        published = a.find('pubDate').text
+        description = a.find('description').text
+        #TODO: Add article to MongoDB
+        # outcome = posts_coll.insert_one()
+        #TODO: Bulk add articles to MongoDB
+        break
+      break
+  except BaseException as inst:
+    print("Oh no, /data_scheduled JOB FAILED.")
+  return True
+
+#TODO: Data Labeling using Embedding Model
+@app.get('/label_scheduled')
+@auth_required_sync
+def embedding_labels(request: Request, client: MongoClient = Depends(get_db_client)):
+
+  #Label each post in DB
+
+  return
